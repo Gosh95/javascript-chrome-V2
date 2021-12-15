61d9e4ff651d84f168212e336c65187108216f93
Gosh95 <pocal777@naver.com>
2021-12-16 00:24:37 +0300

    javascript
    배경 이미지 삭제 깔끔하게 디자인함, 버튼으로 배경 색 바꿀 수 있음(메모지 바꾸는듯 한 느낌)

--- js/background.js
+++ js/background.js
@@ -1,14 +1,31 @@
-const imgFiles = ["0.jpeg", "1.jpeg", "2.jpeg"];
-const imgTag = document.createElement("img");
-imgTag.classList.add("background");
-function changeBackground() {
-    const randomImg = imgFiles[Math.floor(Math.random() * imgFiles.length)];
-    const background = `img/${randomImg}`;
+const body = document.body;
+const leftRemoteBtn = document.querySelector(".background-remote span:first-child");
+const rightRemoteBtn = document.querySelector(".background-remote span:last-child");
+const backgroundColor = ["rgb(252, 250, 249)", "rgb(189, 173, 165)", "rgb(202, 101, 70)",  " rgb(243, 161, 119)", "rgb(197, 76, 16)", "rgb(133, 122, 235)", "rgb(109, 201, 86)"];
 
-    imgTag.src = background;
 
-    document.body.appendChild(imgTag);
+let randomIdx;
+const bgSize = backgroundColor.length;
+function initBg() {
+    randomIdx = Math.floor(Math.random() * bgSize);
+    body.style.backgroundColor = backgroundColor[randomIdx];
 }
 
-changeBackground();
-setInterval(changeBackground, 30000);
+initBg();
+
+function handleLeftBtn() {
+    --randomIdx;
+
+    if(randomIdx < 0) {
+        randomIdx %= bgSize;
+    }
+
+    body.style.backgroundColor = backgroundColor[(bgSize + randomIdx) % bgSize];
+}
+
+function handleRightBtn() {
+    body.style.backgroundColor = backgroundColor[++randomIdx % bgSize];
+}
+
+leftRemoteBtn.addEventListener("click", handleLeftBtn);
+rightRemoteBtn.addEventListener("click", handleRightBtn);
